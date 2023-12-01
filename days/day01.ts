bc2cda11dd6e708a715fac70fc88bb26a15a20ee
turpin_l <turpin_l@etna-alternance.net>
2023-12-01 17:34:45 +0100

    fix: day01

--- days/day01.ts
+++ days/day01.ts
@@ -26,19 +26,22 @@ export default class Day01 extends DayResolver {
       "eight",
       "nine",
     ];
-    const NUMBERS_DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     const input = this.getInput();
-    const numbers = input.map((line) => {
-      const regex = new RegExp(`${NUMBERS_STRING.join("|")}|\\d`, "gi");
-      const numbers = [...line.matchAll(regex)].map((matched) => matched[0]);
-      const convertedNumbers = numbers.map((number) =>
-        NUMBERS_DIGITS.includes(number)
-          ? number
-          : (NUMBERS_STRING.indexOf(number) + 1).toString(),
-      );
-      return convertedNumbers.length
-        ? +(convertedNumbers[0] + convertedNumbers[convertedNumbers.length - 1])
-        : 0;
+    const numbers: number[] = [];
+    input.forEach((line) => {
+      const digits: string[] = [];
+
+      line.split("").forEach((letter, letterIndex) => {
+        if (letter.match(/\d/)) {
+          digits.push(letter);
+        }
+        NUMBERS_STRING.forEach((number, numberIndex) => {
+          if (line.slice(letterIndex).startsWith(number)) {
+            digits.push((numberIndex + 1).toString());
+          }
+        });
+      });
+      numbers.push(+(digits[0] + digits[digits.length - 1]));
     });
 
     return numbers.reduce((prev, curr) => prev + curr);
