024309cac8568a9df12ab8e458924c7fb3f59d9e
turpin_l <turpin_l@etna-alternance.net>
2023-12-05 23:12:08 +0100

    feat: day 5 part 1

--- days/day05.ts
+++ days/day05.ts
@@ -1,17 +1,86 @@
 import { DayResolver } from "../day.base";
-interface CategoryValue {
-  category: string;
-  value: string;
+
+interface MappedRangedValues {
+  destinationRangeStart: number;
+  sourceRangeStart: number;
+  rangeLength: number;
+}
+interface Card {
+  fromCategory: string;
+  toCategory: string;
+  mappedValues: MappedRangedValues[];
 }
 export default class Day05 extends DayResolver {
   constructor(input: string) {
     super(input, "\n\n");
   }
-
+  testMappedRangedValuesWithValue(
+    value: number,
+    mappedRangesValue: MappedRangedValues,
+  ) {
+    return (
+      value >= mappedRangesValue.sourceRangeStart &&
+      value <=
+        mappedRangesValue.sourceRangeStart + mappedRangesValue.rangeLength
+    );
+  }
   solveFirstStar() {
     const input = this.getInput();
-    console.log(input);
-    return 0;
+    const seeds = input[0]
+      .split(": ")[1]
+      .split(" ")
+      .map((number) => +number);
+    const categories: Card[] = [];
+    input.slice(1).forEach((card) => {
+      const [header, ...mappedValues] = card.split("\n");
+      const [fromCategory, _to, toCategory] = header.split(" ")[0].split("-");
+      const categoryMappedValues: Card = {
+        fromCategory: fromCategory!,
+        toCategory: toCategory!,
+        mappedValues: mappedValues.map((value) => {
+          const [destinationRangeStart, sourceRangeStart, rangeLength] = value
+            .split(" ")
+            .map((v) => +v);
+          return {
+            destinationRangeStart,
+            sourceRangeStart,
+            rangeLength,
+          };
+        }),
+      };
+
+      categories.push(categoryMappedValues);
+    });
+    const mappedSeedToLocations = seeds.map((seed) => {
+      let currentValue = seed;
+      categories.forEach((category) => {
+        let findRange = false;
+        let i = 0;
+        while (!findRange && i < category.mappedValues.length) {
+          const currentMappedRangedValues = category.mappedValues[i];
+          if (
+            this.testMappedRangedValuesWithValue(
+              currentValue,
+              currentMappedRangedValues,
+            )
+          ) {
+            findRange = true;
+          } else {
+            i += 1;
+          }
+        }
+        if (findRange) {
+          const finalMappedRangesValues = category.mappedValues[i];
+          const diffFromSource =
+            currentValue - finalMappedRangesValues.sourceRangeStart;
+          currentValue =
+            finalMappedRangesValues.destinationRangeStart + diffFromSource;
+        }
+      });
+      return currentValue;
+    });
+
+    return Math.min(...mappedSeedToLocations);
   }
 
   solveSecondStar(): number {
